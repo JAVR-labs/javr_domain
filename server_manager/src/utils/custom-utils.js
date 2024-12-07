6ced22ee795d3e667b0063895be9857b8e1a38db
TdewT <73232730+TdewT@users.noreply.github.com>
2024-12-07 11:22:39 +0100

    Introduced function simulating SIGINT for Windows

--- server_manager/src/utils/custom-utils.js
+++ server_manager/src/utils/custom-utils.js
@@ -3,6 +3,16 @@ const fs = require("node:fs");
 const {statuses} = require("../lib/globals.js");
 let logStream;
 
+const { ctrlc } = require('ctrlc-windows');
+
+function gracefulShutdown(pid) {
+    if (process.platform === 'win32') {
+        ctrlc(pid);
+    } else {
+        process.kill(pid, 'SIGTERM');
+    }
+}
+
 function killTask(name, PID) {
     if (PID) {
         exec(`taskkill /pid ${PID}`, (error, stdout, stderr) => {
@@ -138,5 +148,6 @@ module.exports = {
     customLog,
     getElementByHtmlID,
     emitDataGlobal,
-    anyServerUsed
+    anyServerUsed,
+    gracefulShutdown
 };
