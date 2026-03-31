e8b85006f5e31c0938ad6145ce7635581344cd66
TdewT <szwej.jakub@gmail.com>
2026-03-31 17:41:31 +0200

    Fix Vulnerabilities and update deprecated features
    - Add eager loading to NavLogo image
    - Update deprecated sass/scss features
       - Use sass:color instead of deprecated functions
       - Change `import` to `use`
    - Update eslint config
    - Fix with breaking changes
    - Fix without breaking changes

--- website/src/components/ui/NavLogo.jsx
+++ website/src/components/ui/NavLogo.jsx
@@ -5,7 +5,8 @@ const logoPath = '/img/logo.png';
 function NavLogo() {
     return (
         <>
-            <a className="navbar-brand" href="/website/public"><Image src={logoPath} width={80} height={80} alt="javr logo"/></a>
+            <a className="navbar-brand" href="/website/public"><Image src={logoPath} width={80} height={80}
+                                                                      loading=eager alt="javr logo"/></a>
         </>
     )
 }
