d67f279f977a74e122f4f947b0e0dbea8fc60a9a
TdewT <szwej.jakub@gmail.com>
2026-04-15 20:05:30 +0200

    refactor: move shared code into separate library
    1. Refactor `customLog` utility to a shared module and update imports
    
    - Migrated `customLog` from `server/utils/custom-utils.cjs` to `shared/Logger.js` for centralized logging.
    - Updated all references to `customLog` across the codebase.
    - Adjusted module alias configurations in `package.json` and `jsconfig.json` to include the new `@shared` path.
    - Updated logger references in website and server_manager
    
    2. Refactor `ConfigManager` to support dependency injection and centralised configuration management
    
    - Decoupled `ConfigTypes` and `FileTemplates` into a new `ConfigSettings` module.
    - Transformed `ConfigManager` into a singleton class that enables dependency injection of `ConfigTypes` and `FileTemplates`.
    - Updated website to use the new `ConfigManager` instantiation and adjusted file imports:
      - Moved `ConfigManager` to `shared` module.
      - Replaced legacy static methods with instance-based calls.
    - Updated server initialization to dynamically include environment variables (cli with --env-file) in website configurations.

--- website/server.cjs
+++ website/server.cjs
@@ -1,27 +1,31 @@
 require('module-alias/register');
-const {customLog} = require("@/server/utils/custom-utils.cjs");
-const {ConfigManager} = require("@server-lib/ConfigManager.cjs");
+const {ConfigManager} = require("@javr-domain/shared/ConfigManager.cjs");
 const ServerInstance = require("@/server/lib/ServerInstance.cjs");
 const ServerManagerList = require("@/server/lib/ServerManagerList.cjs");
 const {initialiseBoards} = require("@server-utils/arduino-utils.cjs");
 const {getWebsiteIO} = require("@server-lib/globals.js");
-const {ConfigTypes} = require("@server-lib/ConfigManager.cjs");
+const {ConfigTypes, FileTemplates} = require("@server-lib/ConfigSettings");
+const {customLog} = require("@javr-domain/shared/Logger.js");
 
 // Name of module used for logs
 const logName = "INIT";
 
 // Get configs
 customLog(logName, "Loading configs");
-ConfigManager.loadConfigs();
+const configManager = new ConfigManager(ConfigTypes, FileTemplates);
+configManager.loadConfigs();
 
 // Website initialisation
-const server = new ServerInstance(ConfigManager.getConfig(ConfigTypes.websiteConfig));
+const websiteConfig = configManager.getConfig(ConfigTypes.websiteConfig);
+const environment = process.env.NODE_ENV;
+const websiteSettings = {...websiteConfig, processEnv: environment};
+const server = new ServerInstance(websiteSettings);
 
 // Arduino initialisation
-initialiseBoards(ConfigManager.getConfig(ConfigTypes.arduinos));
+initialiseBoards(configManager.getConfig(ConfigTypes.arduinos));
 
 
-customLog(logName, "Starting the website server");
+customLog(logName, `Starting the website server in ${environment} mode`);
 server.startWebsite().then(() => {
     customLog(logName, 'Loading Server Managers');
     ServerManagerList.loadServerManagers(getWebsiteIO());
